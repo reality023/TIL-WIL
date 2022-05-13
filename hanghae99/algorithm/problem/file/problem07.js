c185fd89695beb5310dedfe26cb2d77f17ae7bf7
Gridy <reality023@naver.com>
2022-05-13 15:49:33 +0900

    항해 알고리즘 7번 문제 추가

--- hanghae99/algorithm/problem/file/problem07.js
+++ hanghae99/algorithm/problem/file/problem07.js
@@ -1,4 +1,9 @@
 // 7번 문제
-// 음양 더학기
+// 음양 더하기
 // https://programmers.co.kr/learn/courses/30/lessons/76501
 
+function solution(absolutes, signs) {
+    return absolutes.reduce((a, b, i) => a + (signs[i] ? b : -b), 0);
+}
+
+console.log(solution([1, 2, 3], [false, false, true]));
